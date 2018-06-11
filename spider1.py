c1e61cb13b6aa99cef9476c433880d6cce540591
openlab <fu_zhipeng@163.com>
2018-06-11 11:17:23 +0800

    update the spider1

--- spider1.py
+++ spider1.py
@@ -1,8 +1,10 @@
 import re
 import requests
 import time
+import json
 
-class spider():
+
+class Spider():
 	url_eos = 'https://otcbtc.com/sell_offers?currency=eos&fiat_currency=cny&payment_type=all'
 	url_btc = 'https://otcbtc.com/sell_offers?currency=btc&fiat_currency=cny&payment_type=all'
 	url_bch = 'https://otcbtc.com/sell_offers?currency=bch&fiat_currency=cny&payment_type=all'
@@ -13,6 +15,7 @@ class spider():
 		'bch' : url_bch,
 		'eth' : url_eth
 	}
+	
 	regex = r'<div class="recommend-card__price">([\s\S]*?)</div>'#？非贪婪
 
 	def __fetch_content(self,url):
@@ -25,12 +28,23 @@ class spider():
 		result = result[0].strip()
 		return result
 
+	def __save(self,coin,time,price):
+		with open('history_coin_data.json','r+') as f:
+			text = f.read()
+			dict1 = json.loads(text)
+			dict1[coin][time] = price
+			new_text = json.dumps(dict1)
+			f.seek(0)
+			f.write(new_text)
+
+
 	def go(self):
 		for k,v in self.__class__.urls.items():
 			r_text = self.__fetch_content(v)
 			result = self.__analysis(r_text)
 			now = time.asctime( time.localtime(time.time()) )
+			self.__save(k,now,result)
 			print("the price of " + k + " is:￥" + result + "      " + now)
-#test
-spider = spider()
+	
+spider = Spider()
 spider.go()
