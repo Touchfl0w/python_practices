aac4979625b7ced0b86604cba7cf2450d6c2fabf
touchfl0w <1754643407@qq.com>
2018-08-14 22:27:51 +0800

    Update test_email.py

--- modules_learing/smtplib/test_email.py
+++ modules_learing/smtplib/test_email.py
@@ -6,8 +6,8 @@ def mail(sender,passwd,recievers):
 	ret = True
 	try:
 		msg = MIMEText('hello world ','plain','utf-8')
-		msg['From'] = formataddr(("就爱深蓝色",sender))
-		msg['To'] = formataddr(['付志鹏',recievers[0]])
+		msg['From'] = formataddr(("XXX",sender))
+		msg['To'] = formataddr(['XXX',recievers[0]])
 		msg['Subject'] = "邮件测试"
 		server = smtplib.SMTP_SSL('smtp.qq.com',465)
 		print("hello")
@@ -19,9 +19,9 @@ def mail(sender,passwd,recievers):
 	return ret
 
 if __name__ == '__main__':
-	my_sender = '1754643407@qq.com'
-	my_reciever = ['fu_zhipeng@163.com']
-	my_passwd = 'iaytbwfmgbimeieg' 
+	my_sender = 'XXX@qq.com'
+	my_reciever = ['XXX@163.com']
+	my_passwd = 'XXXX' 
 	result = mail(my_sender,my_passwd,my_reciever)
 	print(result)
 	if result:
