0aaa97429934cc0d657ba8fd3c8e699f02a05f44
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 19:28:50 +0000

    Adding tarfile member sanitization to extractall()

--- modules_learing/tarfile/test_tarfile.py
+++ modules_learing/tarfile/test_tarfile.py
@@ -20,7 +20,26 @@ with tarfile.open('test2.tar.gz','w:gz') as tar:
 
 #4抽取所有文件
 with tarfile.open('test1.tar') as tar:
-	tar.extractall(path='./test')
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, path="./test")
 
 import os.path 
 #5 解压并选择性抽取文件
@@ -38,4 +57,23 @@ def jsonfiles(tar):
 with tarfile.open('test2.tar.gz') as tar:
 	#members必须是tarinfo对象的集合
 	#path不存在会自动创建
-	tar.extractall(path='./test1',members=jsonfiles(tar))
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, path="./test1", members=jsonfiles(tar))
